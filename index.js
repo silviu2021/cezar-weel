43c18f2182583edace4025c14d925fb4c4342dbc
silviu2021 <95830272+silviu2021@users.noreply.github.com>
2024-04-27 18:14:24 +0000

    wip

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const alphabetEl = document.querySelector(".alphabet");
 
 const alphabet = Array.from({ length: 26 }, (_, i) =>
-  String.fromCharCode(i + 65)
-);
+  String.fromCharCode(65 + i)
+).reverse();
 
-for (const letter of alphabet) {
+let lastElement = alphabet.pop();
+alphabet.unshift(lastElement);
+
+const angleIncrement = 360 / alphabet.length;
+
+const radius = 50;
+
+const centerX = 50;
+const centerY = 50;
+const distance = 0.9;
+const scaledRadius = radius * distance;
+const startAngle = 180;
+
+console.log(alphabet[0])
+console.log(alphabet[1])
+
+for (let i = 0; i < alphabet.length; i++) {
+  const letter = alphabet[i];
   const letterEl = document.createElement("div");
-  letterEl.classList.add('letter');
+  letterEl.classList.add("letter");
   letterEl.innerHTML = letter;
-  if(letter == "A"){
-    letterEl.style.top="5%";
-    letterEl.style.left="50%";
-  }
+
+  const angleDegrees = startAngle + angleIncrement * i;
+  const angleRadians = (angleDegrees * Math.PI) / 180;
+
+  const x = centerX + scaledRadius * Math.cos(angleRadians);
+  const y = centerY + scaledRadius * Math.sin(angleRadians);
+
+  letterEl.style.top = `${x}%`;
+  letterEl.style.left = `${y}%`;
+
   alphabetEl.appendChild(letterEl);
 }
