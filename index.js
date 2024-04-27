dca62eec28f1f64c4d9441092891c9ecf4f82229
silviu2021 <95830272+silviu2021@users.noreply.github.com>
2024-04-27 23:46:36 +0000

    final

--- index.js
+++ index.js
@@ -1,99 +1,103 @@
-const alphabetEl = document.querySelector(".alphabet");
-const alphabet2El = document.querySelector(".alphabet2");
+setTimeout(() => {
+  const alphabetEl = document.querySelector(".alphabet");
+  const alphabet2El = document.querySelector(".alphabet2");
 
-const alphabet = Array.from({ length: 26 }, (_, i) =>
-  String.fromCharCode(65 + i)
-).reverse();
+  const alphabet = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(65 + i)
+  ).reverse();
 
-alphabet.unshift("_")
+  alphabet.unshift("_");
 
-const angleIncrement = 360 / alphabet.length;
+  const angleIncrement = 360 / alphabet.length;
 
-const radius = 50;
+  const radius = 50;
 
-const centerX = 50;
-const centerY = 50;
-const distance = 0.9;
-const scaledRadius = radius * distance;
-const startAngle = 180;
+  const centerX = 50;
+  const centerY = 50;
+  const distance = 0.9;
+  const scaledRadius = radius * distance;
+  const startAngle = 180;
 
-for (let i = alphabet.length - 1; i >= 0; i--) {
-  const letter = alphabet[i];
-  const letterEl = document.createElement("div");
-  const letterAltEl = document.createElement("div");
-  letterEl.classList.add("letter");
-  letterEl.classList.add("originalLetter");
-  letterAltEl.classList.add("letter");
-  letterAltEl.classList.add("letterAlt");
-  letterEl.innerHTML = letter;
-  letterAltEl.innerHTML = letter;
+  for (let i = alphabet.length - 1; i >= 0; i--) {
+    const letter = alphabet[i];
+    const letterEl = document.createElement("div");
+    const letterAltEl = document.createElement("div");
+    letterEl.classList.add("letter");
+    letterEl.classList.add("originalLetter");
+    letterAltEl.classList.add("letter");
+    letterAltEl.classList.add("letterAlt");
+    letterEl.innerHTML = letter;
+    letterAltEl.innerHTML = letter;
 
-  const angleDegrees = startAngle + angleIncrement * (i + 1);
-  const angleRadians = (angleDegrees * Math.PI) / 180;
+    const angleDegrees = startAngle + angleIncrement * (i + 1);
+    const angleRadians = (angleDegrees * Math.PI) / 180;
 
-  const x = centerX + scaledRadius * Math.cos(angleRadians);
-  const y = centerY + scaledRadius * Math.sin(angleRadians);
+    const x = centerX + scaledRadius * Math.cos(angleRadians);
+    const y = centerY + scaledRadius * Math.sin(angleRadians);
 
-  letterEl.style.setProperty("--x", `${x}%`);
-  letterAltEl.style.setProperty("--x", `${x}%`);
-  letterEl.style.setProperty("--y", `${y}%`);
-  letterAltEl.style.setProperty("--y", `${y}%`);
+    letterEl.style.setProperty("--x", `${x}%`);
+    letterAltEl.style.setProperty("--x", `${x}%`);
+    letterEl.style.setProperty("--y", `${y}%`);
+    letterAltEl.style.setProperty("--y", `${y}%`);
 
-  alphabetEl.appendChild(letterEl);
-  alphabet2El.appendChild(letterAltEl);
+    alphabetEl.appendChild(letterEl);
+    alphabet2El.appendChild(letterAltEl);
+    setTimeout(() => {
+      letterEl.classList.add("loaded");
+    }, 100 * (alphabet.length - i));
+  }
+
   setTimeout(() => {
-    letterEl.classList.add("loaded");
-  }, 100 * (alphabet.length - i));
-}
+    alphabetEl.classList.add("loaded");
+  }, (alphabet.length - 1) * 100 + 4000);
 
-setTimeout(() => {
-  alphabetEl.classList.add("loaded");
-}, (alphabet.length - 1) * 100 + 4000);
+  setTimeout(() => {
+    alphabetEl.classList.add("loaded2");
+    alphabet2El.classList.add("loaded2");
+  }, (alphabet.length - 1) * 100 + 6000);
 
-setTimeout(() => {
-  alphabetEl.classList.add("loaded2");
-  alphabet2El.classList.add("loaded2");
-}, (alphabet.length - 1) * 100 + 6000);
-
-setTimeout(() => {
+  setTimeout(() => {
     alphabetEl.classList.add("loaded3");
     alphabet2El.classList.add("loaded3");
   }, (alphabet.length - 1) * 100 + 8000);
 
-setTimeout(() => {
-  document.body.classList.add("loaded");
-}, 100);
+  setTimeout(() => {
+    document.body.classList.add("loaded");
+  }, 100);
 
-let offset = 0;
+  let offset = 0;
 
-const addBtn = document.querySelector(".add");
-const substractBtn = document.querySelector(".substract");
-const nr = document.querySelector(".number");
+  const addBtn = document.querySelector(".add");
+  const substractBtn = document.querySelector(".substract");
+  const nr = document.querySelector(".number");
 
-const ajjustOffset = () => {
-  const letters = [...document.querySelectorAll(".alphabet .letter")].reverse();
-  for (let i = alphabet.length - 1; i >= 0; i--) {
-    const letterEl = letters[i];
+  const ajjustOffset = () => {
+    const letters = [
+      ...document.querySelectorAll(".alphabet .letter"),
+    ].reverse();
+    for (let i = alphabet.length - 1; i >= 0; i--) {
+      const letterEl = letters[i];
 
-    const angleDegreeso = startAngle + angleIncrement * (i - offset + 1);
-    const angleRadianso = (angleDegreeso * Math.PI) / 180;
+      const angleDegreeso = startAngle + angleIncrement * (i - offset + 1);
+      const angleRadianso = (angleDegreeso * Math.PI) / 180;
 
-    const x = centerX + scaledRadius * Math.cos(angleRadianso);
-    const y = centerY + scaledRadius * Math.sin(angleRadianso);
+      const x = centerX + scaledRadius * Math.cos(angleRadianso);
+      const y = centerY + scaledRadius * Math.sin(angleRadianso);
 
-    letterEl.style.setProperty("--x", `${x}%`);
-    letterEl.style.setProperty("--y", `${y}%`);
-  }
-};
+      letterEl.style.setProperty("--x", `${x}%`);
+      letterEl.style.setProperty("--y", `${y}%`);
+    }
+  };
 
-addBtn.addEventListener("click", () => {
-  offset++;
-  ajjustOffset();
-  nr.innerHTML = offset;
-});
+  addBtn.addEventListener("click", () => {
+    offset++;
+    ajjustOffset();
+    nr.innerHTML = offset;
+  });
 
-substractBtn.addEventListener("click", () => {
-  offset--;
-  ajjustOffset();
-  nr.innerHTML = offset;
-});
+  substractBtn.addEventListener("click", () => {
+    offset--;
+    ajjustOffset();
+    nr.innerHTML = offset;
+  });
+}, 2000);
