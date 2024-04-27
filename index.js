52f5fe8ce8641b7193acbc5076b33323bfef4669
silviu2021 <95830272+silviu2021@users.noreply.github.com>
2024-04-27 19:20:37 +0000

    wip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const alphabetEl = document.querySelector(".alphabet");
+const alphabet2El = document.querySelector(".alphabet2");
 
 const alphabet = Array.from({ length: 26 }, (_, i) =>
   String.fromCharCode(65 + i)
@@ -20,8 +21,12 @@ const startAngle = 180;
 for (let i = 0; i < alphabet.length; i++) {
   const letter = alphabet[i];
   const letterEl = document.createElement("div");
+  const letterAltEl = document.createElement("div");
   letterEl.classList.add("letter");
+  letterAltEl.classList.add("letter");
+  letterAltEl.classList.add("letterAlt");
   letterEl.innerHTML = letter;
+  letterAltEl.innerHTML = letter;
 
   const angleDegrees = startAngle + angleIncrement * i;
   const angleRadians = (angleDegrees * Math.PI) / 180;
@@ -30,12 +35,26 @@ for (let i = 0; i < alphabet.length; i++) {
   const y = centerY + scaledRadius * Math.sin(angleRadians);
 
   letterEl.style.setProperty("--x", `${x}%`);
+  letterAltEl.style.setProperty("--x", `${x}%`);
   letterEl.style.setProperty("--y", `${y}%`);
+  letterAltEl.style.setProperty("--y", `${y}%`);
 
   alphabetEl.appendChild(letterEl);
+  alphabet2El.appendChild(letterAltEl);
   setTimeout(() => {
     letterEl.classList.add("loaded");
   }, 100 * (i + 1));
 }
 
-document.body.classList.add("loaded");
+setTimeout(() => {
+  alphabetEl.classList.add("loaded");
+}, (alphabet.length - 1) * 100 + 4000);
+
+setTimeout(() => {
+    alphabetEl.classList.add("loaded2");
+    alphabet2El.classList.add("loaded2");
+  }, (alphabet.length - 1) * 100 + 6000);
+
+setTimeout(()=>{
+    document.body.classList.add("loaded");
+},100)
