5004776d40cc8888e967534e7ce0351372e87ae9
silviu2021 <95830272+silviu2021@users.noreply.github.com>
2024-05-05 02:18:21 +0300

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,60 @@
-const CACHE_NAME = 'cezar-app-cache-v1';
+const CACHE_NAME = "cezar-app-cache-v1";
 const urlsToCache = [
-//   '/',
-//   '/styles/main.css',
-//   '/scripts/main.js',
-//   '/images/icon.png',
+  "/",
+  "index.css",
+  "index.js",
+  "bg.webp",
   // Add other static assets to cache here
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('fetching....')  
+self.addEventListener("fetch", (event) => {
+  console.log("fetching....");
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // Clone the request
+      const fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
           return response;
         }
 
-        // Clone the request
-        const fetchRequest = event.request.clone();
+        // Clone the response
+        const responseToCache = response.clone();
 
-        return fetch(fetchRequest).then(
-          response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
 
-            // Clone the response
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
+        return response;
+      });
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
-
+  console.log("activating....");
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
