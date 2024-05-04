6cbbf9599072a940a343d80f801d94e8074bb63a
silviu2021 <95830272+silviu2021@users.noreply.github.com>
2024-05-05 02:21:34 +0300

    fix

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,12 @@ const urlsToCache = [
 
 self.addEventListener("install", (event) => {
   // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Opened cache");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
 });
 
 self.addEventListener("fetch", (event) => {
